9e0e0a1bac043748a49966fa3b85a70f19d0cfcc
Ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2014-05-05 17:54:14 +0200

    Clean the code

--- tunnel/tunnel.py
+++ tunnel/tunnel.py
@@ -1,118 +1,29 @@
-import math
 import pygame
 import sys
+import tunnel_sprites
 
 from pygame.locals import *
-from random import choice
 
 BLACK = (0,0,0)
 SPEED = 200000
-START = (310,450)
+WAIT = 1000
 WHITE = (255,255,255)
 window_size = (640,480)
 
-class Cursor(pygame.sprite.Sprite):
+window = pygame.display.set_mode(window_size)
 
-	size = (20,20)
+def display_level(road, cursor):
+	window.fill(WHITE)
+	road.draw(window)
+	window.blit(cursor.image, cursor.rect)
 
-	def __init__(self):
-		pygame.sprite.Sprite.__init__(self)
-		self.image = pygame.Surface(self.size)
-		self.image.fill(WHITE)
-		self.rect = pygame.Rect(START, self.size)
-
-	def move_left(self):
-		if self.rect[0] != 0:
-			self.rect = self.rect.move(-10,0)
-
-	def move_right(self):
-		if self.rect[0] != 620:
-			self.rect = self.rect.move(10,0)
-
-class Chunk(pygame.sprite.Sprite):
-
-	def __init__(self, position, size):
-		self.position = position
-		self.size = size
-
-		pygame.sprite.Sprite.__init__(self)
-		self.image = pygame.Surface(self.size)
-		self.image.fill(BLACK)
-		self.rect = pygame.Rect(position, self.size)
-
-	def  update(self, position, size):
-		self.position = position
-		self.size = size
-
-		self.image = pygame.Surface(self.size)
-		self.image.fill(BLACK)
-		self.rect = pygame.Rect(position, self.size)
-
-class Road(pygame.sprite.Group):
-
-	size = (320,20)
-	position = (160,0)
-
-	chunk_list = []
-
-	def __init__(self):
-		pygame.sprite.Group.__init__(self)
-
-		for i in range(24):
-			self.chunk_list.append(Chunk((160,20 * i), self.size))
-			self.add(self.chunk_list[i])
-
-	def calc_size(self, input):
-		result = (int((280/math.exp(input/5000.0))+40), 20)
-		return result
-
-	def calc_position(self, input):
-		pos_seq = [-10,-5, 5, 10]
-
-		if (input[0]+self.size[0]+5) >= window_size[0]:
-			result = ((input[0]-10),0)
-
-		elif (input[0]-5) <= 0:
-			result = ((input[0]+10),0)
-
-		else:
-			result = ((input[0]+choice(pos_seq)),0)
-
-		return result
-
-	def defill(self, score):
-		self.size = self.calc_size(score)
-		self.position = self.calc_position(self.position)
-
-		self.chunk_list.insert(0, Chunk(self.position, self.size))
-		self.add(self.chunk_list[0])
-
-		len_chunk_list = len(self.chunk_list)
-		for i in range(len_chunk_list):
-			position = (self.chunk_list[i].position[0], 20 * i)
-			self.chunk_list[i].update(position, self.chunk_list[i].size)
-
-			if i >= 24:
-				self.remove(self.chunk_list[i])
-				self.chunk_list.pop(i)
-
-
-	def collide(self, cursor):
-		collide_list = []
-
-		collide_list = pygame.sprite.spritecollide(cursor, self, False, pygame.sprite.collide_rect_ratio(0.9))
-		if not collide_list:
-			return False
-		else:
-			return True
-
-def display_score(score, window):
+def display_score(score):
 	font = pygame.font.Font(None, 30)
 	text = font.render(str(score), True, BLACK, WHITE)
 	textpos = window.get_rect().topleft
 	window.blit(text, textpos)
 
-def display_info(msg, window):
+def display_info(msg):
 	font = pygame.font.Font(None,50)
 
 	if msg == 1:
@@ -128,22 +39,23 @@ def display_info(msg, window):
 		textpos = window.get_rect().center
 		window.blit(text, textpos)
 
-def main():
+def init():
 	pygame.init()
+	pygame.event.set_blocked(pygame.MOUSEMOTION)
 	pygame.key.set_repeat(1,0)
 
+def run():
+	run = True
 	score = 0
 
-	window = pygame.display.set_mode(window_size)
-	cursor = Cursor()
-	road = Road()
-
-	run = True
+	cursor = tunnel_sprites.Cursor()
+	road = tunnel_sprites.Road()
 
 	while run:
 		for event in pygame.event.get():
 			if event.type == QUIT:
-				run = False
+				pygame.quit()
+				sys.quit()
 
 			if event.type == KEYDOWN:
 				if event.key == K_LEFT:
@@ -156,26 +68,33 @@ def main():
 			if i == 0:
 				road.defill(score)
 
-
-
-		window.fill(WHITE)
-		road.draw(window)
-		window.blit(cursor.image, cursor.rect)
-		
-		display_score(score, window)
+		display_level(road, cursor)
 
 		score += 1
-
-		if not road.collide(cursor):
-			display_info(3, window)
-			break
+		display_score(score)
 
 		pygame.display.flip()
 
-	pygame.display.flip()
-	
-	event = pygame.event.wait()
-	if event.type == pygame.KEYDOWN:
-		print 'restart'  
+		if not road.collide(cursor):
+			display_info(3)
+			pygame.display.flip()
+			run = False
+
+def main():
+	init()
+
+	new_game = True
+	while new_game:
+		run()
+		new_game = False
+
+		event = pygame.event.wait()
+
+		if event.type == QUIT:
+			pygame.quit()
+			sys.quit()
+		if event.type == pygame.KEYDOWN:
+			new_game = True
+			continue
 
 main()
---
+++ tunnel/tunnel_sprites.py
@@ -0,0 +1,107 @@
+import math
+import pygame
+import sys
+
+from pygame.locals import *
+from random import choice
+
+BLACK = (0,0,0)
+START = (310,450)
+WHITE = (255,255,255)
+window_size = (640,480)
+
+
+class Cursor(pygame.sprite.Sprite):
+
+	size = (20,20)
+
+	def __init__(self):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.Surface(self.size)
+		self.image.fill(WHITE)
+		self.rect = pygame.Rect(START, self.size)
+
+	def move_left(self):
+		if self.rect[0] != 0:
+			self.rect = self.rect.move(-10,0)
+
+	def move_right(self):
+		if self.rect[0] != 620:
+			self.rect = self.rect.move(10,0)
+
+class Chunk(pygame.sprite.Sprite):
+
+	def __init__(self, position, size):
+		self.position = position
+		self.size = size
+
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.Surface(self.size)
+		self.image.fill(BLACK)
+		self.rect = pygame.Rect(position, self.size)
+
+	def  update(self, position, size):
+		self.position = position
+		self.size = size
+
+		self.image = pygame.Surface(self.size)
+		self.image.fill(BLACK)
+		self.rect = pygame.Rect(position, self.size)
+
+class Road(pygame.sprite.Group):
+
+	size = (320,20)
+	position = (160,0)
+
+	chunk_list = []
+
+	def __init__(self):
+		pygame.sprite.Group.__init__(self)
+
+		for i in range(24):
+			self.chunk_list.append(Chunk((160,20 * i), self.size))
+			self.add(self.chunk_list[i])
+
+	def calc_size(self, input):
+		result = (int((280/math.exp(input/5000.0))+40), 20)
+		return result
+
+	def calc_position(self, input):
+		pos_seq = [-10,-5, 5, 10]
+
+		if (input[0]+self.size[0]+5) >= window_size[0]:
+			result = ((input[0]-10),0)
+
+		elif (input[0]-5) <= 0:
+			result = ((input[0]+10),0)
+
+		else:
+			result = ((input[0]+choice(pos_seq)),0)
+
+		return result
+
+	def defill(self, score):
+		self.size = self.calc_size(score)
+		self.position = self.calc_position(self.position)
+
+		self.chunk_list.insert(0, Chunk(self.position, self.size))
+		self.add(self.chunk_list[0])
+
+		for i,v in enumerate(self.chunk_list):
+			position = (v.position[0], 20 * i)
+			v.update(position, v.size)
+
+			if i >= 24:
+				self.remove(v)
+				self.chunk_list.pop(i)
+
+
+	def collide(self, cursor):
+		collide_list = []
+
+		collide_list = pygame.sprite.spritecollide(
+			cursor, self, False, pygame.sprite.collide_rect_ratio(0.9))
+		if not collide_list:
+			return False
+		else:
+			return True
